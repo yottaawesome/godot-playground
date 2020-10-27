37afc54b4771d48465fc5f2d171cc3a256a33c6a
Vasilios Magriplis <etameson@gmail.com>
2020-10-27 19:56:17 +1100

    More LoonyLips

--- src/Loony_Lips/LoonyLips.gd
+++ src/Loony_Lips/LoonyLips.gd
@@ -1,17 +1,31 @@
 extends Control
 
 var player_words = []
-var story = "Once upon a time, %s played %s and it made him all %s inside. It was a %s day."
-var prompts = ["a name", "a noun", "an adverb", "adjective"]
+var template = [
+	{
+		"prompts": ["a name", "a noun", "an adverb", "adjective"],
+		"story": "Once upon a time, %s played %s and it made him all %s inside. It was a %s day."
+	},
+	{
+		"prompts": ["a name", "a noun", "an adjective"],
+		"story": "Here is %s, he is a %s who is %s."
+	}
+]
+var current_story = null	
 
 onready var player_text = $VBoxContainer/HBoxContainer/PlayerText
 onready var button_label = $VBoxContainer/HBoxContainer/ButtonLabel
 onready var display_text = $VBoxContainer/DisplayText
 
 func _ready():
+	set_current_story()
 	display_text.text = "Welcome to LoonyLips! "
 	check_player_word_length()
 	player_text.grab_focus()
+
+func set_current_story():
+	randomize()
+	current_story = template[randi() % template.size()]
 
 
 func _on_PlayerText_text_entered(_new_text):
@@ -33,7 +47,7 @@ func add_to_player_words():
 
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 
 
 func check_player_word_length():
@@ -44,11 +58,11 @@ func check_player_word_length():
 
 
 func tell_story():
-	display_text.text = story % player_words
+	display_text.text = current_story.story % player_words
 
 
 func prompt_player():
-	display_text.text += "May I have " + prompts[player_words.size()] + " please?"
+	display_text.text += "May I have " + current_story.prompts[player_words.size()] + " please?"
 
 
 func end_game():
