97ae553618a0c598bb2cd93cd046cbedfd435ff0
Vasilios Magriplis <etameson@gmail.com>
2020-11-01 19:37:33 +1100

    More HoppyDays

--- src/Hoppy Days/Player/Player.gd
+++ src/Hoppy Days/Player/Player.gd
@@ -7,6 +7,8 @@ const GRAVITY = 300
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 5000
 
+signal animate
+
 func _physics_process(delta):
 	apply_gravity()
 	jump()
@@ -38,17 +40,7 @@ func apply_gravity():
 		motion.y = 0
 	else:
 		motion.y += GRAVITY
-		
-		
+
+
 func animate():
-	if motion.y < 0:
-		$AnimatedSprite.play("jump");
-	elif motion.x < 0:
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("walk")
-	elif motion.x > 0:
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("walk")
-	else:
-		$AnimatedSprite.play("idle")
-	
+	emit_signal("animate", motion)
